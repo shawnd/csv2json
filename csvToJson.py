35aa9b2d508f95ef5dcfdc436dd60acd90a45145
Per-Henrik Kvalnes <perhenrik@nordlandssykehuset.no>
2014-07-31 18:55:25 +0200

    script has pased first test

--- csvToJson.py
+++ csvToJson.py
@@ -3,9 +3,49 @@
 # @author Per-Henrik Kvalnes
 
 import sys
+import json
 
 infile = open("test.csv")
-outfile = "test.json"
+outfile = open("out.json", "w")
+
+first = 0
+header = {}
+
+jsonList = []
 
 for line in infile:
-    print line
+
+    # if first time, prepare the header 
+    if not first:
+        line = line.strip("\n")
+        keys = line.split(";")
+
+        i = 0
+        for key in keys:
+            if key != "":
+                header[i] = key
+                i += 1
+
+
+
+        first = 1
+
+    # if noe, make an json object
+    else:
+        obj = {}
+        line = line.strip("\n")
+        values = line.split(";")
+
+        i = 0
+        for value in values:
+            if value != "":
+                obj[header[i]] = value
+
+                i += 1
+
+        jsonList.append(obj)
+
+jsonString = json.dumps(jsonList, indent=4)
+
+outfile.write(jsonString)
+outfile.close()
